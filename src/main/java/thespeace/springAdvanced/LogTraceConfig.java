33438616e3437ef646f9915cead82d37d37574eb
thespeace <thespeace1@gmail.com>
2024-06-01 16:55:37 +0900

    FEAT: ThreadLocal - apply

--- src/main/java/thespeace/springAdvanced/LogTraceConfig.java
+++ src/main/java/thespeace/springAdvanced/LogTraceConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import thespeace.springAdvanced.trace.logtrace.FieldLogTrace;
 import thespeace.springAdvanced.trace.logtrace.LogTrace;
+import thespeace.springAdvanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        //동시성 문제가 있는 FieldLogTrace대신에 문제를 해결한 ThreadLocalLogTrace를 스프링 빈으로 등록.
+//        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
