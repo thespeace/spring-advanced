9814124933ab8ef7c0ac16be3d074e0f9de8c2b1
thespeace <thespeace1@gmail.com>
2024-06-08 22:19:34 +0900

    FEAT: Concrete class-based proxy - Apply

---
+++ src/main/java/thespeace/springAdvanced/proxy/config/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
@@ -0,0 +1,38 @@
+package thespeace.springAdvanced.proxy.config.v1_proxy.concrete_proxy;
+
+import thespeace.springAdvanced.proxy.app.v2.OrderControllerV2;
+import thespeace.springAdvanced.trace.TraceStatus;
+import thespeace.springAdvanced.trace.logtrace.LogTrace;
+
+public class OrderControllerConcreteProxy extends OrderControllerV2 {
+
+    private final OrderControllerV2 target;
+    private final LogTrace logTrace;
+
+    public OrderControllerConcreteProxy(OrderControllerV2 target, LogTrace logTrace) {
+        super(null);
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderController.request()");
+
+            //target 호출
+            String result = target.request(itemId);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+    @Override
+    public String noLog() {
+        return target.noLog();
+    }
+}
