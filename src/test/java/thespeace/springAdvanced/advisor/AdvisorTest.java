6a272d758eeed41aa297d2d3881f27a6d4da4631
thespeace <thespeace1@gmail.com>
2024-06-15 15:28:59 +0900

    TEST: Proxy supported by Spring - Example code 2(Create pointcut)

--- src/test/java/thespeace/springAdvanced/advisor/AdvisorTest.java
+++ src/test/java/thespeace/springAdvanced/advisor/AdvisorTest.java
@@ -1,6 +1,10 @@
 package thespeace.springAdvanced.advisor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -8,14 +12,16 @@ import thespeace.springAdvanced.proxy.common.advice.TimeAdvice;
 import thespeace.springAdvanced.proxy.common.service.ServiceImpl;
 import thespeace.springAdvanced.proxy.common.service.ServiceInterface;
 
-/**
- * <h1>어드바이저 - 예제 코드 1</h1>
- * 어드바이저는 하나의 포인트컷과 하나의 어드바이스를 가지고 있다.<br>
- * 프록시 팩토리를 통해 프록시를 생성할 때 어드바이저를 제공하면 어디에 어떤 기능을 제공할 지 알 수 있다.<p><p>
- */
+import java.lang.reflect.Method;
+
+@Slf4j
 public class AdvisorTest {
 
     /**
+     * <h2>어드바이저 - 예제 코드 1</h2>
+     * 어드바이저는 하나의 포인트컷과 하나의 어드바이스를 가지고 있다.<br>
+     * 프록시 팩토리를 통해 프록시를 생성할 때 어드바이저를 제공하면 어디에 어떤 기능을 제공할 지 알 수 있다.<p>
+     *
      * <ul>
      *     <li>new DefaultPointcutAdvisor : Advisor 인터페이스의 가장 일반적인 구현체이다.
      *         생성자를 통해 하나의 포인트컷과 하나의 어드바이스를 넣어주면 된다.
@@ -45,5 +51,100 @@ public class AdvisorTest {
 
         proxy.save();
         proxy.find();
+    }
+
+    /**
+     * <h2>직접 만든 포인트컷 - 예제 코드 2</h2>
+     *
+     * <h3>포인트컷이 적용되어야 하는 경우(그림으로 정리)</h3>
+     * <ol>
+     *     <li>클라이언트가 프록시의 save() 를 호출한다.</li>
+     *     <li>포인트컷에 Service 클래스의 save() 메서드에 어드바이스를 적용해도 될지 물어본다.</li>
+     *     <li>포인트컷이 true 를 반환한다. 따라서 어드바이스를 호출해서 부가 기능을 적용한다.</li>
+     *     <li>이후 실제 인스턴스의 save() 를 호출한다.</li>
+     * </ol>
+     *
+     * <h3>포인트컷이 적용되지 않는 경우(그림으로 정리)</h3>
+     * <ol>
+     *     <li>클라이언트가 프록시의 find() 를 호출한다.</li>
+     *     <li>포인트컷에 Service 클래스의 find() 메서드에 어드바이스를 적용해도 될지 물어본다.</li>
+     *     <li>포인트컷이 false 를 반환한다. 따라서 어드바이스를 호출하지 않고, 부가 기능도 적용되지 않는다.</li>
+     *     <li>실제 인스턴스를 호출한다.</li>
+     * </ol>
+     */
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    /**
+     * <h2>직접 구현한 포인트컷</h2>
+     * 현재 메서드 기준으로 로직을 적용하면 된다.
+     * 클래스 필터는 항상 true 를 반환하도록 했고, 메서드 비교 기능은 직접 구현한 MyMethodMatcher 를 사용한다.
+     *
+     * @see Pointcut org.springframework.aop.Pointcut 인터페이스를 구현
+     * @see MyMethodMatcher 직접 구현한 MethodMatcher
+     */
+    static class MyPointcut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    /**
+     * <h2>직접 구현한 MethodMatcher</h2>
+     * <ul>
+     *     <li>isRuntime() , matches(... args) : isRuntime() 이 값이 참이면 matches(... args) 메서드가 대신 호출된다.
+     *                                           동적으로 넘어오는 매개변수를 판단 로직으로 사용할 수 있다.
+     *         <ul>
+     *             <li>isRuntime() 이 false 인 경우 클래스의 정적 정보만 사용하기 때문에 스프링이 내부에서 캐싱을 통해
+     *                 성능 향상이 가능하지만, isRuntime() 이 true 인 경우 매개변수가 동적으로 변경된다고 가정하기
+     *                 때문에 캐싱을 하지 않는다.</li>
+     *             <li>크게 중요한 부분은 아니니 참고만 하고 넘어가자.</li>
+     *         </ul>
+     *     </li>
+     * </ul>
+     * @see MethodMatcher org.springframework.aop.MethodMatcher 인터페이스를 구현
+     */
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        /**
+         * 해당 메서드에 method, targetClass 정보가 넘어온다.<br>
+         * 이 정보로 어드바이스를 적용할지 적용하지 않을지 판단할 수 있다.
+         */
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName); //"save"인 경우 true 반환하도록 판단 로직을 적용.
+            log.info("포인트컷 호출 method={} targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과 result={}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
